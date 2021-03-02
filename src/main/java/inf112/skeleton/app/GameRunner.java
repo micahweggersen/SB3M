c6bd12e8ae43278a4147b304f6ea488aea632363
Maria Bonde <wuf009@uib.no>
2021-03-02 13:22:32 +0100

    endringer etter codewithme

--- src/main/java/inf112/skeleton/app/GameRunner.java
+++ src/main/java/inf112/skeleton/app/GameRunner.java
@@ -1,6 +1,6 @@
 package inf112.skeleton.app;
 
-import inf112.skeleton.app.Cards.Deck;
+import com.badlogic.gdx.Input;import inf112.skeleton.app.Cards.Deck;
 import inf112.skeleton.app.Cards.Cards;
 
 import java.util.ArrayList;
@@ -9,6 +9,7 @@ public class GameRunner {
 
     private static Deck deck = new Deck();
     private static ArrayList<Cards> programmedCards = new ArrayList<>();
+    private static ArrayList<Cards> chosenCards = new ArrayList<>();
 
     public static void main(String[] args) {
         Deck deck = new Deck();
@@ -25,5 +26,41 @@ public class GameRunner {
 
     public GameRunner() { }
 
-    public static void dealCards(){ programmedCards = deck.getCards(5); }
+    public static void dealCards(){
+        programmedCards = deck.getCards(9);
+    }
+
+
+    public void keyUp(int keycode){
+
+        if (keycode == Input.Keys.NUM_1) {
+            chosenCards.add(programmedCards.get(0));
+        }
+        if (keycode == Input.Keys.NUM_2) {
+            chosenCards.add(programmedCards.get(1));
+        }
+        if (keycode == Input.Keys.NUM_3) {
+            chosenCards.add(programmedCards.get(2));
+        }
+        if (keycode == Input.Keys.NUM_4) {
+            chosenCards.add(programmedCards.get(3));
+        }
+        if (keycode == Input.Keys.NUM_5) {
+            chosenCards.add(programmedCards.get(4));
+        }
+        if (keycode == Input.Keys.NUM_6) {
+            chosenCards.add(programmedCards.get(5));
+        }
+        if (keycode == Input.Keys.NUM_7) {
+            chosenCards.add(programmedCards.get(6));
+        }
+        if (keycode == Input.Keys.NUM_8) {
+            chosenCards.add(programmedCards.get(7));
+        }
+        if (keycode == Input.Keys.NUM_9) {
+            chosenCards.add(programmedCards.get(8));
+        }
+
+
+    }
 }
