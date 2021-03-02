919356ea39324e4573db32cc0a97aca2bf85920d
Sondre <sondrerasmussen@live.no>
2021-03-02 15:01:52 +0100

    Tekniske problemer

--- src/main/java/inf112/skeleton/app/Gamerunner.java
+++ src/main/java/inf112/skeleton/app/Gamerunner.java
@@ -1,8 +1,28 @@
-//package inf112.skeleton.app;
-//
-//public class Gamerunner extends Movement {
-//
-//
-//
-//
-//}
+package inf112.skeleton.app;
+
+import inf112.skeleton.app.Cards.Deck;
+import inf112.skeleton.app.Cards.UpdateCards;
+
+import java.util.ArrayList;
+
+public class GameRunner {
+
+    private static Deck deck = new Deck();
+    private static ArrayList<UpdateCards> programmedCards = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.createDeck();
+
+        dealCards();
+        System.out.println(programmedCards.get(0).toString());
+        System.out.println(programmedCards.get(1).toString());
+        System.out.println(programmedCards.get(2).toString());
+        System.out.println(programmedCards.get(3).toString());
+        System.out.println(programmedCards.get(4).toString());
+    }
+
+    public GameRunner() { }
+
+    public static void dealCards(){ programmedCards = deck.getCards(5); }
+}
