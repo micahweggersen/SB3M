92e70857dc57817bd5aa1d4710526475e1d05145
Maria Bonde <wuf009@uib.no>
2021-03-05 12:18:59 +0100

    tester til kortklassen skrevet av Malin

--- src/test/java/inf112/skeleton/app/Cards/TestCards.java
+++ src/test/java/inf112/skeleton/app/Cards/TestCards.java
@@ -12,7 +12,11 @@ public class TestCards {
 
     Cards moveTwoCard = new MoveTwo(270, "Move Two", Cards.ROTATE_0, 2);
     Cards rotateRightCard = new RotateRight(80, "Rotate Right", Cards.ROTATE_90, 0);
-
+    Cards moveOneCard = new MoveOne(270, "Move One", Cards.ROTATE_0, 1);
+    Cards moveThreeCard = new MoveThree(270, "Move Three", Cards.ROTATE_0, 3);
+    Cards backUpCard = new BackUp(180, "Back Up", Cards.ROTATE_0, -1);
+    Cards rotateLeftCard = new RotateLeft(180, "Rotate Left", Cards.ROTATE_270, 0);
+    Cards uTurnCard = new UTurn(270, "U-Turn", Cards.ROTATE_180, 0);
 
     @Test
     public void testGetValueOfMoveTwoCard(){
@@ -60,6 +64,42 @@ public class TestCards {
     public void testGetMomentumOfRotateRightCard(){
         int momentum = rotateRightCard.getMomentum();
         assertEquals(0, momentum);
+    }
+
+    @Test
+    public void getValueOfMoveOneCard(){
+        int value = moveOneCard.getPriorityValue();
+        assertEquals(270, value);
+    }
+
+    @Test
+    public void getIdOfMoveOneCard(){
+        String id = moveOneCard.getId();
+        assertEquals("Move One", id);
+    }
+
+    @Test
+    public void getValueOfMoveThreeCard(){
+        int value = moveThreeCard.getPriorityValue();
+        assertEquals(270, value);
+    }
+
+    @Test
+    public void getValueOfBackUpCard(){
+        int value = backUpCard.getPriorityValue();
+        assertEquals(180, value);
+    }
+
+    @Test
+    public void getValueOfRotateLeftCard(){
+        int value = rotateLeftCard.getPriorityValue();
+        assertEquals(180, value);
+    }
+
+    @Test
+    public void getValueOfUTurnCard(){
+        int value = uTurnCard.getPriorityValue();
+        assertEquals(270, value);
     }
 
 
