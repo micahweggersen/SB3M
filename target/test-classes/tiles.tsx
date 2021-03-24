4f64dc453891b0ec5a04271dc5effcc37d65329c
Maria Bonde <wuf009@uib.no>
2021-03-24 13:35:42 +0100

    unversioned files

---
+++ target/test-classes/tiles.tsx
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tiles" tilewidth="300" tileheight="300" tilecount="136" columns="8">
+ <image source="tiles.png" width="2400" height="5100"/>
+ <tile id="7">
+  <properties>
+   <property name="EAST" value=""/>
+   <property name="SOUTH" value=""/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="EAST" value=""/>
+   <property name="NORTH" value=""/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="EAST" value=""/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="NORTH" value=""/>
+   <property name="WEST" value=""/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="SOUTH" value=""/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="WEST" value=""/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="NORTH" value=""/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="SOUTH" value=""/>
+   <property name="WEST" value=""/>
+  </properties>
+ </tile>
+</tileset>
